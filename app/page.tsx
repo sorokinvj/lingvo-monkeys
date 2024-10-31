777975310169ea350571af96ef973ce5eaa72a41
Vladislav Sorokin <sorokinvj@gmail.com>
2024-10-31 22:48:30 +0000

    feat: meta

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import LandingPage from '@/components/landing/LandingPage';
 
+export const metadata = {
+  title: 'Be Monkey',
+  description: 'Listen to your audience',
+};
+
 export default async function Index() {
   return <LandingPage />;
 }
