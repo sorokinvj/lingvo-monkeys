82e7a5b3cc1011ffaeb0bb56917b7c3674deb638
Vladislav Sorokin <sorokinvj@gmail.com>
2025-01-19 20:39:50 +0000

    fix: header

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,6 @@
 'use client';
 import { FC, useState, useEffect } from 'react';
 import Link from 'next/link';
-import { Button } from '@/components/ui/button';
-import { Drawer } from '@/components/ui/drawer';
-
-import Image from 'next/image';
 import { useUser } from '@/hooks/useUser';
 
 export const Header: FC = () => {
@@ -45,41 +41,31 @@ export const Header: FC = () => {
     <header
       className={`
       bg-white z-20 shadow-header md:relative w-full md:max-w-6xl md:mx-auto md:translate-x-0 md:right-auto rounded-full mx-auto
-      transition-all duration-300 ease-in-out
+      transition-all duration-300 ease-in-out flex items-center justify-between px-6 py-4
       ${isCollapsed ? 'max-md:!w-14 max-md:!right-4 max-md:!left-auto max-md:!translate-x-0 max-md:!mx-0 max-md:!p-0' : ''}
     `}
     >
-      <nav
-        className={`w-full flex flex-col items-center px-6 py-4 justify-center gap-4
-        transition-all duration-300 ease-in-out
-        ${isCollapsed ? 'max-md:!p-4' : ''}
-      `}
-      >
-        <div
-          className={`
-          transition-all duration-300 ease-in-out
-          ${isCollapsed ? 'max-md:w-0 max-md:opacity-0' : 'w-auto opacity-100'}
-        `}
+      <Link href="/">
+        <h1
+          className={`text-xl font-bold  text-black
+          ${isCollapsed ? 'max-md:hidden' : ''}`}
         >
-          <Link href="/">
-            <h1 className="text-xl font-bold bg-gradient-to-r from-violet-500 via-fuchsia-500 to-pink-500 text-transparent bg-clip-text">
-              Lingvo Monkeys
-            </h1>
-          </Link>
-        </div>
-        <div className="flex items-center gap-8">
-          {Object.entries(navConfig).map(
-            ([key, value]) =>
-              ('enabled' in value ? value.enabled : true) && (
-                <Link href={value.href} key={key}>
-                  <div className="text-base text-gray-90 hover:text-violet-500">
-                    {value.title}
-                  </div>
-                </Link>
-              )
-          )}
-        </div>
-      </nav>
+          Lingvo Monkeys
+        </h1>
+      </Link>
+
+      <div className="flex items-center gap-8">
+        {Object.entries(navConfig).map(
+          ([key, value]) =>
+            ('enabled' in value ? value.enabled : true) && (
+              <Link href={value.href} key={key}>
+                <div className="text-base text-gray-90 hover:text-violet-500">
+                  {value.title}
+                </div>
+              </Link>
+            )
+        )}
+      </div>
     </header>
   );
 };
