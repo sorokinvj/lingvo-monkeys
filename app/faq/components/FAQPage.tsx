f042ca5cacbc8dc65ede088c70148b1a42fd0ee7
Vladislav Sorokin <sorokinvj@gmail.com>
2025-02-10 22:02:11 +0000

    fix: faq

--- app/faq/components/FAQPage.tsx
+++ app/faq/components/FAQPage.tsx
@@ -5,13 +5,11 @@ import FAQ from './FAQ';
 
 const FAQPage: FC = () => {
   return (
-    <div className="max-w-[1080px] mx-auto p-6 md:px-4 flex flex-col md:flex-row gap-4 md:gap-20 mb-24 relative z-10">
+    <div className="md:mt-12 p-4 md:px-4 flex flex-col md:flex-row gap-8 md:gap-20 mb-24 relative z-10">
       <div className="flex flex-col max-w-[420px] w-full justify-between">
-        <div>
-          <h2 className="font-heading text-4xl leading-tight font-semibold text-gray-900 mt-3 md:text-5xl md:leading-tight">
-            Часто задаваемые вопросы
-          </h2>
-        </div>
+        <h2 className="font-heading text-4xl leading-tight  text-gray-900 mt-3 md:text-5xl md:leading-tight">
+          Часто задаваемые вопросы
+        </h2>
       </div>
       <FAQ />
     </div>
