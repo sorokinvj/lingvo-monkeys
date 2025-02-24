01a01d95110c2f80f6b51f986ffa1c8bde48b51c
Vladislav Sorokin <sorokinvj@gmail.com>
2025-02-24 13:45:48 +0000

    Feat/recomendaton (#11)
    * feat: use accordeon for reco
    
    * fix: bg
    
    * fix: mobile
    
    * fix: build

--- app/(withFooter)/f-a-q/page.tsx
+++ app/(withFooter)/f-a-q/page.tsx
@@ -1,10 +1,9 @@
-import FAQPage from './components/FAQPage';
+import { FAQPage } from './components/FAQPage';
 
 export const metadata = {
   title: 'FAQ | Lingvomonkeys',
 };
 
 export default async function FAQ() {
-  console.log('FAQ page');
   return <FAQPage />;
 }
---
+++ components/FAQItem/FAQItem.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react';
+import { Disclosure, DisclosureButton } from '@headlessui/react';
+import { MinusIcon, PlusIcon } from 'lucide-react';
+
+interface FAQItemProps {
+  question: string;
+  answer: string | React.ReactNode;
+  defaultOpen?: boolean;
+  bgColor?: string;
+}
+
+export const FAQItem: FC<FAQItemProps> = ({
+  question,
+  answer,
+  defaultOpen = false,
+  bgColor = 'bg-gray-50',
+}) => {
+  return (
+    <Disclosure defaultOpen={defaultOpen}>
+      {({ open }) => (
+        <div className="flex flex-col shadow-disclosure rounded-lg overflow-hidden">
+          <DisclosureButton
+            className={`flex justify-between w-full px-4 md:p-7 py-3 text-base font-medium text-left text-gray-900 ${bgColor} focus:outline-none`}
+          >
+            <span className="font-semibold">{question}</span>
+            {open ? (
+              <MinusIcon className="w-5 h-5 text-[#534CFB] transition-transform duration-200" />
+            ) : (
+              <PlusIcon className="w-5 h-5 text-gray-500 transition-transform duration-200" />
+            )}
+          </DisclosureButton>
+
+          <div
+            className={`transition-all duration-300 ease-in-out ${
+              open ? 'max-h-96' : 'max-h-0'
+            } overflow-hidden`}
+          >
+            <div className={`p-4 pt-0 text-base text-gray-500 ${bgColor}`}>
+              {answer}
+            </div>
+          </div>
+        </div>
+      )}
+    </Disclosure>
+  );
+};
