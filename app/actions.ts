2384330021d871811cc766fc42422662707bfcd8
Vladislav Sorokin <sorokinvj@gmail.com>
2025-03-23 21:52:46 +0000

    fix: reset link

--- app/actions.ts
+++ app/actions.ts
@@ -60,7 +60,7 @@ export const forgotPasswordAction = async (formData: FormData) => {
   }
 
   const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: `${origin}/auth/callback?redirect_to=/reset-password`,
+    redirectTo: `${origin}/auth/callback?redirect_to=/reset-password&type=recovery`,
   });
 
   if (error) {
